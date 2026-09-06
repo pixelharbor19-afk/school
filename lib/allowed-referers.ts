df9231c5d846fcdd324996b54fa686c05312bf98
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-06 08:32:43 +0800

    vidstuck cors

--- lib/allowed-referers.ts
+++ lib/allowed-referers.ts
@@ -2,30 +2,14 @@ const ALLOWED_REFERERS = [
   "http://192.168.1.4:3000/",
   "http://localhost:3000/",
   //
-  "https://player.zxcstream.xyz/",
-  "https://player.zxcprime.xyz/",
-
-  //BACKUP
-  "https://backup-zxcstream-xyz.up.railway.app/",
-  "https://backup-zxcprime-xyz.up.railway.app/",
-  "https://online-seller-tau.vercel.app/",
-  "https://backup-zxcstream.up.railway.app/",
-  "https://backup-zxcstream.netlify.app/",
+  "https://vidstuck.xyz/",
 ];
 
 export const ALLOWED_ORIGINS = [
   "http://192.168.1.4:3000",
   "http://localhost:3000",
   //
-  "https://player.zxcstream.xyz",
-  "https://player.zxcprime.xyz",
-
-  //BACKUP
-  "https://backup-zxcstream-xyz.up.railway.app",
-  "https://backup-zxcprime-xyz.up.railway.app",
-  "https://online-seller-tau.vercel.app",
-  "https://backup-zxcstream.up.railway.app",
-  "https://backup-zxcstream.netlify.app",
+  "https://vidstuck.xyz",
 ];
 export function isValidReferer(referer: string): boolean {
   return ALLOWED_REFERERS.some((allowed) => referer.includes(allowed));
