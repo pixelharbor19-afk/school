5cd8fc20ee2622d68f2a836bc6fdf941e02214f8
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-07 08:52:42 +0800

    fix title

--- app/embed/[...params]/player-controls.tsx
+++ app/embed/[...params]/player-controls.tsx
@@ -181,7 +181,7 @@ export default function VideoControls({
               >
                 <ChevronLeft className="md:size-8 size-6" strokeWidth={3} />
                 <span className="text-left">
-                  <h1 className="tracking-wide md:font-medium text-sm line-clamp-1">
+                  <h1 className="tracking-wide font-medium md:text-base text-sm line-clamp-1">
                     {title}
                   </h1>
                 </span>
