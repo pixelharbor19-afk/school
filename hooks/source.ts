197aa2f42f2752a69af9280d2b114f487d14b341
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-18 15:40:33 +0800

    rotate

--- hooks/source.ts
+++ hooks/source.ts
@@ -100,7 +100,7 @@ export function sourceQueryOptions(
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/npminstall", {
+      const { data: token } = await axios.post("/backend/typescript", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: path,
--- hooks/subs.ts
+++ hooks/subs.ts
@@ -32,7 +32,7 @@ export default function useSubtitle({
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/npminstall", {
+      const { data: token } = await axios.post("/backend/typescript", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: "subtitle",
