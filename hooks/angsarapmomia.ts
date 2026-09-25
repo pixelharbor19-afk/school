6abbab606d74c360505d34b7c3fcb42bb8d3b669
jerometecson-main <zxctest8@zxcstream.xyz>
2026-09-26 01:11:29 +0800

    rotate

--- hooks/angsarapmomia.ts
+++ hooks/angsarapmomia.ts
@@ -100,7 +100,7 @@ export function sourceQueryOptions(
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/github", {
+      const { data: token } = await axios.post("/backend/facebook", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: path,
--- hooks/subs.ts
+++ hooks/subs.ts
@@ -32,7 +32,7 @@ export default function useSubtitle({
     refetchOnReconnect: false,
 
     queryFn: async () => {
-      const { data: token } = await axios.post("/backend/github", {
+      const { data: token } = await axios.post("/backend/facebook", {
         [FIELD_MAP.id]: tmdbId,
         [FIELD_MAP.mediaType]: media_type,
         [FIELD_MAP.path]: "subtitle",
